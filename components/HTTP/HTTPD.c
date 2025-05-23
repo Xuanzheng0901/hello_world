ebeff02130bfa274c76bcea3f4e437d6bdd1d69b
Xuanzheng0901 <szx1691196289@gmail.com>
2025-05-23 20:56:42 +0800

    温度显示、音量调节

--- components/HTTP/HTTPD.c
+++ components/HTTP/HTTPD.c
@@ -1,6 +1,6 @@
 #include "HTTP.h"
 
-extern bool power_status[2];
+extern bool power_status[2], auto_control_volume;
 extern float s_temperature;
 
 extern const uint8_t index_html_start[] asm("_binary_index_html_gz_start");
@@ -26,11 +26,28 @@ esp_err_t get_power_info(httpd_req_t *req)
     httpd_resp_send(req, &buf, 1);
     return ESP_OK;
 }
+
 esp_err_t get_temp_info(httpd_req_t *req)
 {
     char buf[32];
     snprintf(buf, sizeof(buf), "%.2f", s_temperature);
-    ESP_LOGI("TEMP", "TEMP INFO GOT");
+    //ESP_LOGI("TEMP", "TEMP INFO GOT");
+    httpd_resp_send(req, buf, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+esp_err_t get_res(httpd_req_t *req)
+{
+    char buf[16];
+    snprintf(buf, sizeof(buf), "%d", wiper_pos);
+    httpd_resp_send(req, buf, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+esp_err_t get_auto(httpd_req_t *req)
+{
+    char buf[16];
+    snprintf(buf, sizeof(buf), "%d", auto_control_volume);
     httpd_resp_send(req, buf, HTTPD_RESP_USE_STRLEN);
     return ESP_OK;
 }
@@ -74,10 +91,24 @@ esp_err_t pwrctrl_post_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
+esp_err_t autoctrl_post_handler(httpd_req_t *req)
+{
+    char *buf = (char*)malloc(req->content_len*sizeof(char)+1);
+    httpd_req_recv(req, buf, req->content_len);
+    buf[req->content_len] = 0;
+
+    auto_control_volume = atoi(buf);
+
+    free(buf);
+    httpd_resp_send(req, "Success", HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
 void HTTP_Init(void)
 {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.lru_purge_enable = true;
+    config.max_uri_handlers = 10;
     config.uri_match_fn = httpd_uri_match_wildcard;
     ESP_LOGI(TAG, "Starting server on port: %d", config.server_port);
     ESP_ERROR_CHECK(httpd_start(&server, &config));
@@ -99,10 +130,22 @@ void HTTP_Init(void)
         .handler = get_power_info,
         .user_ctx  = NULL
     };
+    const httpd_uri_t get_auto_status = {
+        .uri = "/auto_status",
+        .method = HTTP_GET,
+        .handler = get_auto,
+        .user_ctx  = NULL
+    };
     const httpd_uri_t get_temp = {
         .uri = "/temp",
         .method = HTTP_GET,
         .handler = get_temp_info,
+        .user_ctx  = NULL
+    };
+    const httpd_uri_t get_res_pos = {
+        .uri = "/respos",
+        .method = HTTP_GET,
+        .handler = get_res,
         .user_ctx  = NULL
     };
     const httpd_uri_t post = {
@@ -117,10 +160,19 @@ void HTTP_Init(void)
         .handler = pwrctrl_post_handler,
         .user_ctx  = NULL
     };
+    const httpd_uri_t auto_ctr = {
+        .uri = "/auto",
+        .method = HTTP_POST,
+        .handler = autoctrl_post_handler,
+        .user_ctx  = NULL
+    };
     httpd_register_uri_handler(server, &get);
     httpd_register_uri_handler(server, &get_icon);
     httpd_register_uri_handler(server, &post);
     httpd_register_uri_handler(server, &get_power);
     httpd_register_uri_handler(server, &get_temp);
     httpd_register_uri_handler(server, &post_pwr_ctr);
+    httpd_register_uri_handler(server, &get_res_pos);
+    httpd_register_uri_handler(server, &auto_ctr);
+    httpd_register_uri_handler(server, &get_auto_status);
 }
